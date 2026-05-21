8df2c4dc746da132d496dacaa8cd43b9cac200ae
Michael Mazur <michaelmazur89@gmail.com>
2026-05-21 11:09:19 -0500

    chore: initialize Xcode project

---
+++ IntervalTimer/IntervalTimer/IntervalTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntervalTimerApp.swift
+//  IntervalTimer
+//
+//  Created by Michael Mazur on 5/21/26.
+//
+
+import SwiftUI
+
+@main
+struct IntervalTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
